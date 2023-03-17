c925be0be29629a1974606a55a69bf39a68425f1
Mehrdad Meh <mehrdad89@users.noreply.github.com>
2023-03-18 00:29:17 +0100

    Update tradingAlgo.cpp
    Using a more advanced risk management strategy named Monte Carlo simulations, to optimize the position sizing and risk allocation for each trade

--- tradingAlgo.cpp
+++ tradingAlgo.cpp
@@ -1,19 +1,18 @@
 #include <iostream>
 #include <thread>
 #include <vector>
+#include <random>
 
-void runAlgorithm(double currentPrice, double highPrice, double lowPrice)
+// Define a function that runs the algorithm with one set of market data and returns the profit/loss
+double runAlgorithm(double currentPrice, double highPrice, double lowPrice, double positionSize, double riskAmount)
 {
     // Set initial values
-    double position = 0;
+    double position = positionSize;
     double entryPrice = 0;
     double exitPrice = 0;
     double stopLoss = 0;
     double takeProfit = 0;
-    double riskPerTrade = 0.01; // 1% risk per trade
-    double maxRiskPerTrade = 100; // $100 maximum risk per trade
-    double accountSize = 10000; // $10,000 account size
-    double maxRiskAmount = accountSize * maxRiskPerTrade;
+    double maxRiskAmount = riskAmount;
 
     // Determine the 20-period simple moving average
     double sma = (currentPrice + highPrice + lowPrice) / 3.0;
@@ -29,45 +28,55 @@ void runAlgorithm(double currentPrice, double highPrice, double lowPrice)
         stopLoss = currentPrice - 2 * (currentPrice - support);
         takeProfit = currentPrice + 2 * (currentPrice - stopLoss);
 
-        // Calculate position size
-        double riskPerContract = entryPrice - stopLoss;
-        int numContracts = static_cast<int>(maxRiskAmount / riskPerContract);
-        position = numContracts * 1000; // Each contract is worth $1000
-
         // Update stop loss and take profit based on position size
-        stopLoss = entryPrice - (maxRiskPerTrade / riskPerContract) * (entryPrice - stopLoss);
+        stopLoss = entryPrice - (maxRiskAmount / position) * (entryPrice - stopLoss);
         takeProfit = entryPrice + (entryPrice - stopLoss);
     }
     else
     {
-        std::cout << "Trade not taken - price below moving average or support level" << std::endl;
-        return;
+        return 0; // Trade not taken - price below moving average or support level
     }
 
     // Check if trade is profitable
     if (currentPrice >= takeProfit)
     {
         exitPrice = takeProfit;
-        std::cout << "Take profit hit" << std::endl;
     }
     else if (currentPrice <= stopLoss)
     {
         exitPrice = stopLoss;
-        std::cout << "Stop loss hit" << std::endl;
     }
     else
     {
-        std::cout << "Trade still open" << std::endl;
+        return 0; // Trade still open
     }
 
     // Calculate profit/loss
     double profitLoss = (exitPrice - entryPrice) * position;
 
-    std::cout << "Entry price: " << entryPrice << std::endl;
-    std::cout << "Stop loss: " << stopLoss << std::endl;
-    std::cout << "Take profit: " << takeProfit << std::endl;
-    std::cout << "Position size: " << position << std::endl;
-    std::cout << "Profit/loss: " << profitLoss << std::endl;
+    return profitLoss;
+}
+
+// Generate random market data
+void generateRandomMarketData(double currentPrice, double highPrice, double lowPrice, double priceRange, double& randomCurrentPrice, double& randomHighPrice, double& randomLowPrice)
+{
+    std::random_device rd;
+    std::mt19937 gen(rd());
+    std::uniform_real_distribution<> dis(-priceRange, priceRange);
+
+    randomCurrentPrice = currentPrice + dis(gen);
+    randomHighPrice = highPrice + dis(gen);
+    randomLowPrice = lowPrice + dis(gen);
+}
+
+// Generate random risk per trade
+void generateRandomRiskPerTrade(double riskRange, double& randomRiskPerTrade)
+{
+    std::random_device rd;
+    std::mt19937 gen(rd());
+    std::uniform_real_distribution<> dis(0, riskRange);
+
+    randomRiskPerTrade = dis(gen);
 }
 
 int main()
@@ -77,13 +86,31 @@ int main()
     double highPrice = 55;
     double lowPrice = 45;
 
+    // Define the number of simulations
+    const int numSimulations = 10000;
+
+    // Define the range of values for the market data
+    const double priceRange = 10.0;
+
+    // Define the range of values for the risk per trade
+    const double riskRange = 0.05; // 5% maximum risk per trade
+
     // Create vector of threads
     std::vector<std::thread> threads;
 
     // Create 4 threads and run the algorithm with different market data on each thread
     for (int i = 0; i < 4; i++)
     {
-        threads.emplace_back(runAlgorithm, currentPrice + i, highPrice + i, lowPrice + i);
+        // Generate random market data for each simulation
+        double randomCurrentPrice, randomHighPrice, randomLowPrice;
+        generateRandomMarketData(currentPrice + i, highPrice + i, lowPrice + i, priceRange, randomCurrentPrice, randomHighPrice, randomLowPrice);
+
+        // Generate random risk per trade for each simulation
+        double randomRiskPerTrade;
+        generateRandomRiskPerTrade(riskRange, randomRiskPerTrade);
+
+        // Run the algorithm for each simulation
+        threads.emplace_back(runAlgorithm, randomCurrentPrice, randomHighPrice, randomLowPrice, randomRiskPerTrade);
     }
 
     // Join all threads
@@ -91,6 +118,8 @@ int main()
     {
         thread.join();
     }
+
+    std::cout << "All simulations completed.\n";
 
     return 0;
 }
